bae37210edb23248e5229596a90fa3a34d5f4f28
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-18 15:54:16 +0900

    ComponentScan과 Autowired 시작하기

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // scan 대상에서 제외
+)
+public class AutoAppConfig {
+
+}

bc6ab78ec71137e48cb5ad0310fc19080d6dfabe
LAPTOP-S7Q8KMSK\ip323 <ip323@naver.com>
2021-12-19 10:38:39 +0900

    Bean 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,8 +11,12 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan(
         basePackages = "hello.core",
         basePackageClasses = AutoAppConfig.class,
-        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // scan 대상에서 제외
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // scan 대상에서 제외
 )
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }

7627465f31b04210353c13ea723462ac347d2af9
yrcho <ip323@naver.com>
2022-07-21 20:26:40 +0900

    빈 등록 초기화 및 소멸 - 빈 등록 시 지정

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -19,7 +19,10 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifecycleConfig {
 
-        @Bean
+        // destroyMethod 를 따로 지정하지 않을 경우 기본값으로 추론 (close, shutdown)하여 실행시켜준다 - 외부 라이브러리 대부분 두 이름 중 하나으 메소드를 사용하기 때문
+        // 소멸 기능을 사용하기 싫으면 destroyMethod = "" 공백으로 지정하면 된다.
+        @Bean(initMethod = "init")
+//        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");

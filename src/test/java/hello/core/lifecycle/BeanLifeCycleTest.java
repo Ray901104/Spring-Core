cb6c5e6065929aaf65918f292a1ce1735e0fc08f
yrcho <ip323@naver.com>
2022-07-21 20:30:51 +0900

    빈 등록 초기화 및 소멸 - 애노테이션 @PostConstruct, @PreDestroy < 사용 권장!

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -21,8 +21,9 @@ public class BeanLifeCycleTest {
 
         // destroyMethod 를 따로 지정하지 않을 경우 기본값으로 추론 (close, shutdown)하여 실행시켜준다 - 외부 라이브러리 대부분 두 이름 중 하나으 메소드를 사용하기 때문
         // 소멸 기능을 사용하기 싫으면 destroyMethod = "" 공백으로 지정하면 된다.
-        @Bean(initMethod = "init")
+//        @Bean(initMethod = "init")
 //        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
